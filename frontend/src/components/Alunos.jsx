d3248c345813fa8a9369d21fa040dc8a2c52d743
claudiney63 <claudineyryan75@gmail.com>
2024-06-10 15:08:13 -0300

    chore: Update npm dependencies for frontend package.json

--- frontend/src/components/Alunos.jsx
+++ frontend/src/components/Alunos.jsx
@@ -1,36 +1,43 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import alunos from "../data/alunos.json";
-import turmasData from "../data/turmas.json";
+import axios from "axios";
 
 const Alunos = () => {
-  const [students, setStudents] = useState(alunos);
+  const [students, setStudents] = useState([]);
   const [filteredStudents, setFilteredStudents] = useState([]);
   const [turmas, setTurmas] = useState([]);
   const [selectedTurma, setSelectedTurma] = useState("");
   const navigate = useNavigate();
 
   const alunoTurma = (alunoId) => {
-    const aluno = alunos.find((aluno) => aluno._id === alunoId);
-    const turma = turmasData.find((turma) => turma._id === aluno.turma);
+    const aluno = students.find((aluno) => aluno._id === alunoId);
+    const turma = turmas.find((turma) => turma._id === aluno.turma);
     return turma ? turma.nome : "Turma não encontrada";
   };
 
   const nomeTurma = (turmaId) => {
-    const turma = turmasData.find((turma) => turma._id === turmaId);
+    const turma = turmas.find((turma) => turma._id === turmaId);
     return turma ? turma.nome : "Turma não encontrada";
-  
-  }
+  };
 
   useEffect(() => {
-    const studentsData = [...students];
-    setStudents(studentsData);
-    setFilteredStudents(studentsData);
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("https://sistema-de-notas-one.vercel.app/alunos");
+        const data = response.data;
+        setStudents(data);
+        setFilteredStudents(data);
 
-    const uniqueTurmas = [
-      ...new Set(studentsData.map((student) => student.turma)),
-    ];
-    setTurmas(uniqueTurmas);
+        const uniqueTurmas = [
+          ...new Set(data.map((student) => student.turma)),
+        ];
+        setTurmas(uniqueTurmas);
+      } catch (error) {
+        console.error("Erro ao buscar dados:", error);
+      }
+    };
+
+    fetchData();
   }, []);
 
   const handleTurmaChange = (event) => {
@@ -50,53 +57,51 @@ const Alunos = () => {
   };
 
   return (
-    <>
-      <div className="container mt-5">
-        <h2 className="mb-4">Lista de Alunos</h2>
+    <div className="container mt-5">
+      <h2 className="mb-4">Lista de Alunos</h2>
 
-        <div className="mb-3">
-          <label htmlFor="turmaFilter" className="form-label">
-            Filtrar por Turma:
-          </label>
-          <select
-            id="turmaFilter"
-            className="form-select"
-            value={selectedTurma}
-            onChange={handleTurmaChange}
-          >
-            <option value="">Todas as Turmas</option>
-            {turmas.map((turma) => (
-              <option key={turma} value={turma}>
-                {nomeTurma(turma)}
-              </option>
-            ))}
-          </select>
-        </div>
-
-        <table className="table table-bordered">
-          <thead>
-            <tr>
-              <th>Nome</th>
-              <th>Turma</th>
-              <th>Turno</th>
-            </tr>
-          </thead>
-          <tbody>
-            {filteredStudents.map((student) => (
-              <tr
-                key={student._id}
-                onClick={() => handleRowClick(student._id)}
-                style={{ cursor: "pointer" }}
-              >
-                <td>{student.nome}</td>
-                <td>{alunoTurma(student._id)}</td>
-                <td>{student.turno}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+      <div className="mb-3">
+        <label htmlFor="turmaFilter" className="form-label">
+          Filtrar por Turma:
+        </label>
+        <select
+          id="turmaFilter"
+          className="form-select"
+          value={selectedTurma}
+          onChange={handleTurmaChange}
+        >
+          <option value="">Todas as Turmas</option>
+          {turmas.map((turma) => (
+            <option key={turma} value={turma}>
+              {nomeTurma(turma)}
+            </option>
+          ))}
+        </select>
       </div>
-    </>
+
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Nome</th>
+            <th>Turma</th>
+            <th>Turno</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredStudents.map((student) => (
+            <tr
+              key={student._id}
+              onClick={() => handleRowClick(student._id)}
+              style={{ cursor: "pointer" }}
+            >
+              <td>{student.nome}</td>
+              <td>{alunoTurma(student._id)}</td>
+              <td>{student.turno}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
