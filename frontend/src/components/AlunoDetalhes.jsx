fe0972cac23a048d3c932cea1bb15d6b87e6fb15
claudiney63 <claudineyryan75@gmail.com>
2024-07-23 18:42:33 -0300

    Refactor MateriaDetalhes component to remove isEditing state and handleEditToggle function

--- frontend/src/components/AlunoDetalhes.jsx
+++ frontend/src/components/AlunoDetalhes.jsx
@@ -1,28 +1,21 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 import Swal from "sweetalert2";
-import { useNavigate } from "react-router-dom";
 
 const AlunoDetalhes = () => {
   const { id } = useParams();
   const [aluno, setAluno] = useState(null);
-  const [materias, setMaterias] = useState([]);
-  const [turmas, setTurmas] = useState([]);
   const navigate = useNavigate();
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const [alunoResponse, materiasResponse, turmasResponse] = await Promise.all([
+        const [alunoResponse] = await Promise.all([
           axios.get(`https://sistema-de-notas-one.vercel.app/alunos/${id}`),
-          axios.get("https://sistema-de-notas-one.vercel.app/materias"),
-          axios.get("https://sistema-de-notas-one.vercel.app/turmas"),
         ]);
 
         setAluno(alunoResponse.data);
-        setMaterias(materiasResponse.data);
-        setTurmas(turmasResponse.data);
       } catch (error) {
         console.error("Erro ao buscar dados:", error);
       }
@@ -71,6 +64,10 @@ const AlunoDetalhes = () => {
     return <div>Aluno não encontrado</div>;
   }
 
+  const getNotaStyle = (nota) => ({
+    backgroundColor: nota < 6 ? "lightcoral" : "lightblue",
+  });
+
   return (
     <div className="container mt-5">
       <h2 className="mb-4">{aluno.nome}</h2>
@@ -111,67 +108,86 @@ const AlunoDetalhes = () => {
         <tbody>
           {aluno.notas.map((nota) => {
             const bimestres = nota.bimestres;
-            const mediasBimestrais = bimestres.map(
-              (bimestre) => {
-                // Pega as duas primeiras notas
-                const primeirasNotas = bimestre.notas.slice(0, 2);
-                // Calcula a média das duas primeiras notas
-                const somaNotas = primeirasNotas.reduce((a, b) => a + b, 0);
-                return somaNotas / primeirasNotas.length; // Calcula a média
-              }
-            );
-            const mediaSemestral1 = (mediasBimestrais[0] + mediasBimestrais[1]) / 2;
-            const mediaSemestral2 = (mediasBimestrais[2] + mediasBimestrais[3]) / 2;
-            const mediaFinal = (mediaSemestral1 + mediaSemestral2) / 2;
-
-            const getNotaStyle = (nota) => ({
-              backgroundColor: nota < 6 ? 'lightcoral' : 'lightblue',
+            const mediasBimestrais = bimestres.map((bimestre) => {
+              // Pega as duas primeiras notas
+              const primeirasNotas = bimestre.notas.slice(0, 2);
+              // Calcula a média das duas primeiras notas
+              const somaNotas = primeirasNotas.reduce((a, b) => a + b, 0);
+              const media = somaNotas / primeirasNotas.length; // Calcula a média
+              // Verifica se a nota de recuperação é maior que a média
+              const recuperacaoMaior = bimestre.notas[2] > media;
+              // Substitui a média pela nota de recuperação se esta for maior
+              return { media: recuperacaoMaior ? bimestre.notas[2] : media, recuperacaoMaior };
             });
 
+            const mediaSemestral1 = (mediasBimestrais[0].media + mediasBimestrais[1].media) / 2;
+            const mediaSemestral2 = (mediasBimestrais[2].media + mediasBimestrais[3].media) / 2;
+            const mediaFinal = (mediaSemestral1 + mediaSemestral2) / 2;
+
             return (
-              <tr key={nota.materia}>
+              <tr key={nota.materia._id}>
                 <td>{nota.materia.nome}</td>
                 {bimestres.slice(0, 1).map((bimestre, bimestreIndex) => (
                   <React.Fragment key={`bimestre1-${bimestreIndex}`}>
                     {bimestre.notas.map((n, notaIndex) => (
-                      <td key={`nota1-${bimestreIndex}-${notaIndex}`}>
-                        {n}
-                      </td>
+                      <td key={`nota1-${bimestreIndex}-${notaIndex}`}>{n}</td>
                     ))}
                   </React.Fragment>
                 ))}
-                <td style={getNotaStyle(mediasBimestrais[0])}>{mediasBimestrais[0].toFixed(1)}</td>
+                <td
+                  style={{
+                    ...getNotaStyle(mediasBimestrais[0].media),
+                    backgroundColor: mediasBimestrais[0].recuperacaoMaior ? "yellow" : getNotaStyle(mediasBimestrais[0].media).backgroundColor,
+                  }}
+                >
+                  {mediasBimestrais[0].media.toFixed(1)}
+                </td>
                 {bimestres.slice(1, 2).map((bimestre, bimestreIndex) => (
-                  <React.Fragment key={`bimestre1-${bimestreIndex}`}>
+                  <React.Fragment key={`bimestre2-${bimestreIndex}`}>
                     {bimestre.notas.map((n, notaIndex) => (
-                      <td key={`nota1-${bimestreIndex}-${notaIndex}`}>
-                        {n}
-                      </td>
+                      <td key={`nota2-${bimestreIndex}-${notaIndex}`}>{n}</td>
                     ))}
                   </React.Fragment>
                 ))}
-                <td style={getNotaStyle(mediasBimestrais[1])}>{mediasBimestrais[1].toFixed(1)}</td>
+                <td
+                  style={{
+                    ...getNotaStyle(mediasBimestrais[1].media),
+                    backgroundColor: mediasBimestrais[1].recuperacaoMaior ? "yellow" : getNotaStyle(mediasBimestrais[1].media).backgroundColor,
+                  }}
+                >
+                  {mediasBimestrais[1].media.toFixed(1)}
+                </td>
                 <td style={getNotaStyle(mediaSemestral1)}>{mediaSemestral1.toFixed(1)}</td>
                 {bimestres.slice(2, 3).map((bimestre, bimestreIndex) => (
-                  <React.Fragment key={`bimestre2-${bimestreIndex}`}>
+                  <React.Fragment key={`bimestre3-${bimestreIndex}`}>
                     {bimestre.notas.map((n, notaIndex) => (
-                      <td key={`nota2-${bimestreIndex}-${notaIndex}`}>
-                        {n}
-                      </td>
+                      <td key={`nota3-${bimestreIndex}-${notaIndex}`}>{n}</td>
                     ))}
                   </React.Fragment>
                 ))}
-                <td style={getNotaStyle(mediasBimestrais[2])}>{mediasBimestrais[2].toFixed(1)}</td>
+                <td
+                  style={{
+                    ...getNotaStyle(mediasBimestrais[2].media),
+                    backgroundColor: mediasBimestrais[2].recuperacaoMaior ? "yellow" : getNotaStyle(mediasBimestrais[2].media).backgroundColor,
+                  }}
+                >
+                  {mediasBimestrais[2].media.toFixed(1)}
+                </td>
                 {bimestres.slice(3, 4).map((bimestre, bimestreIndex) => (
-                  <React.Fragment key={`bimestre2-${bimestreIndex}`}>
+                  <React.Fragment key={`bimestre4-${bimestreIndex}`}>
                     {bimestre.notas.map((n, notaIndex) => (
-                      <td key={`nota2-${bimestreIndex}-${notaIndex}`}>
-                        {n}
-                      </td>
+                      <td key={`nota4-${bimestreIndex}-${notaIndex}`}>{n}</td>
                     ))}
                   </React.Fragment>
                 ))}
-                <td style={getNotaStyle(mediasBimestrais[3])}>{mediasBimestrais[3].toFixed(1)}</td>
+                <td
+                  style={{
+                    ...getNotaStyle(mediasBimestrais[3].media),
+                    backgroundColor: mediasBimestrais[3].recuperacaoMaior ? "yellow" : getNotaStyle(mediasBimestrais[3].media).backgroundColor,
+                  }}
+                >
+                  {mediasBimestrais[3].media.toFixed(1)}
+                </td>
                 <td style={getNotaStyle(mediaSemestral2)}>{mediaSemestral2.toFixed(1)}</td>
                 <td style={getNotaStyle(mediaFinal)}>{mediaFinal.toFixed(1)}</td>
                 <td style={getNotaStyle(mediaFinal)}>{mediaFinal < 6 ? "R" : "A"}</td>
